532c599fc75e9f422ac5f09a920cda61c878cbd1
kazikame <saksham47@hotmail.com>
2023-04-12 00:21:15 -0500

    updated model

--- work-stealing/sjf_model.py
+++ work-stealing/sjf_model.py
@@ -13,20 +13,18 @@ class Event:
         self.o = o
 
         self.time = o.Real(f'{id}_time')
+        self.free = o.Bool(f'{id}_free')
         self.task_id = [o.Bool(f'{id}_task_id={i}') for i in range(NT)]
         self.ready_tasks = [o.Bool(f'{id}_ready_t={i}') for i in range(NT)]
         self.lengths = [o.Real(f'{id}_len_t={i}') for i in range(NT)]
 
         # Output
         self.next_task = [o.Bool(f'{id}_next_t={i}') for i in range(NT)]
-    
-    def add_constraints(self):
-        o = self.o
+        o.add(AtMost(*self.next_task, 1))
 
-        o.add(Atmost(next_task, 1))
 
 def get_events(o : MySolver, prefix : str):
-    events = [Event(o, f"{prefix}_event"+str(i)) for i in range(NUM_EVENTS)]
+    events = [Event(o, f"{prefix}_event{i}") for i in range(NUM_EVENTS)]
 
     for i in range(NUM_EVENTS-1):
         o.add(events[i].time <= events[i+1].time)
@@ -42,8 +40,8 @@ def sjf(o : MySolver, events : [Event]):
                 *[Implies(event.ready_tasks[t2], event.lengths[t2] >= event.lengths[t]) for t2 in range(NT)])
             shortest_task.append(constraint)
 
-        # if there's a ready task, there should be a next task
-        o.add(Implies(Or(*event.ready_tasks), Or(*event.next_task)))
+        # if there's a ready task and proc is free, there should be a next task
+        o.add(And(event.free, Or(*event.ready_tasks)) == Or(*event.next_task))
 
         # next task should be the shortest task
         for t in range(NT):
@@ -61,7 +59,14 @@ class Workload:
 
         for t in range(NT):
             o.add(self.ready_time[t] >= 0)
-            o.add(self.lengths[t] >= 0)
+            o.add(self.lengths[t] > 0)
+        
+        # o.add(And(
+        #     self.ready_time[0] == 0,
+        #     self.ready_time[1] == 0,
+        #     self.lengths[0] == 0,
+        #     self.lengths[1] == 10,
+        # ))
 
 
 def add_system(o : MySolver, w : Workload, events : [Event], id : str):
@@ -72,15 +77,26 @@ def add_system(o : MySolver, w : Workload, events : [Event], id : str):
     for e in range(NUM_EVENTS):
         o.add(o.ExactlyK(events[e].task_id, 1))
 
+
     for t in range(NT):
         o.add(Or(*[And(e.time == end_time[t], e.task_id[t]) for e in events]))
         o.add(Or(*[And(e.time == ready_time[t], e.task_id[t]) for e in events]))
     
     # end_time depends on lengths and when it was scheduled
     for t in range(NT):
-        o.add(And(*[Implies(e.next_task[t], end_time[t] == e.time + e.lengths[t]) for e in events]))
+        o.add(And(*[Implies(e.next_task[t], end_time[t] == e.time + w.lengths[t]) for e in events]))
 
-    # lengths should be consistent and positive
+    # free : if the last scheduled task has ended
+    end_time_of_last_task = [o.Real(f"{id}_end_time_lt_{e}") for e in range(NUM_EVENTS)]
+    o.add(end_time_of_last_task[0] == 0)
+    for e in range(1, NUM_EVENTS):
+        o.add(And(*[Implies(events[e-1].next_task[t], end_time_of_last_task[e] == end_time[t]) for t in range(NT)]))
+        o.add(Implies(Not(Or(*events[e-1].next_task)), end_time_of_last_task[e] == end_time_of_last_task[e-1]))
+
+    for e in range(NUM_EVENTS):
+        o.add(events[e].free == (end_time_of_last_task[e] <= events[e].time))
+
+    # lengths
     for i in range(NUM_EVENTS):
         o.add(And(*[events[i].lengths[t] == w.lengths[t] for t in range(NT)]))
 
@@ -90,19 +106,26 @@ def add_system(o : MySolver, w : Workload, events : [Event], id : str):
             o.add(events[e].ready_tasks[t] == And(ready_time[t] <= events[e].time, 
                                                   Not(Or(*[events[prev].next_task[t] for prev in range(0, e)]))))
 
+    ## Physical Constraints
     # Next task must be ready
     for e in range(NUM_EVENTS):
-        o.add(And(*[Implies(events[e].next_task[t], events[e].time >= ready_time[t]) for t in range(NT)]))   
+        o.add(And(*[Implies(events[e].next_task[t], events[e].ready_tasks[t]) for t in range(NT)]))   
 
     # Every task must be scheduled
     for t in range(NT):
-        o.add(Or(*[e.next_task[t] for e in events]))   
+        o.add(Or(*[e.next_task[t] for e in events]))
+
+    # If proc isn't free, can't schedule
+    # Work Conserving: If proc is free and a task is ready, must schedule one
+    for e in range(NUM_EVENTS):
+        o.add(Implies(Not(events[e].free), Not(Or(*events[e].next_task))))
+        o.add(Implies(And(events[e].free, Or(*events[e].ready_tasks)), Or(*events[e].next_task)))
 
 def get_total_time(o : MySolver, w : Workload, events : [Event], prefix : str):
     start_time = [o.Real(f"{prefix}_start_time_t={i}") for i in range(NT)]
 
     for e in events:
-        o.add(And(*[Implies(e.next_task[t], start_time[t] == e.time) for t in range(NT)]))
+        o.add(And(*[Implies(e.next_task[t], start_time[t] == e.time + w.lengths[t]) for t in range(NT)]))
     
     total_wait_time = o.Real(f"{prefix}_total_time")
     o.add(total_wait_time == sum([start_time[t] - w.ready_time[t] for t in range(NT)]))
@@ -112,11 +135,12 @@ if __name__ == "__main__":
     o = MySolver()
     w = Workload(o)
     w.add_constraints()
-
+    # print(o.check())
     events = get_events(o, "sjf")
+    # print(o.check())
     sjf(o, events)
+    # print(o.check())
     add_system(o, w, events, "sjf")
-
     print(o.check())
 
     events_opt = get_events(o, "opt")
@@ -126,6 +150,13 @@ if __name__ == "__main__":
 
     tt_sjf = get_total_time(o, w, events, "sjf")
     tt_opt = get_total_time(o, w, events_opt, "opt")
-    o.add(tt_sjf > tt_opt)
+    o.add(tt_sjf > 2*tt_opt)
     print(o.check())
-    print(o.model())
+    m = o.model()
+
+    strs = []
+    for k in m:
+        strs.append(f"{k}={m[k]}")
+
+    for s in sorted(strs):
+        print(s)
